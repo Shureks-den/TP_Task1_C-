5ef3390b90138e5bd630ee3bc7cac1344c471bf2
Alexander Klonov <alxender.klonov@mail.ru>
2021-03-13 20:45:20 +0300

    Fix main.c

--- src/main.c
+++ src/main.c
@@ -11,7 +11,7 @@ int main(int argc, char* argv[]) {
     if (strcmp(argv[1], "-c") == 0) {
         int num = atoi(argv[3]);
         error_code = create_new_database(argv[2], num);
-        if (!error_code) {
+        if (error_code) {
             return error_code; 
         }
     }
@@ -19,7 +19,7 @@ int main(int argc, char* argv[]) {
     if (strcmp(argv[1], "-a") == 0) {
         int num = atoi(argv[3]);
         error_code = add_to_existing_data_base(argv[2], num);
-        if (!error_code) {
+        if (error_code) {
             return error_code; 
         }
     }
@@ -28,11 +28,11 @@ int main(int argc, char* argv[]) {
         main_list_t* structure = NULL;
         structure = read_from_database(argv[2]);
         error_code = print_everything(structure);
-        if (!error_code) {
+        if (error_code) {
             return error_code;
         }
-        error_code = clear_everything(structure);
-        if (!error_code) {
+        clear_everything(structure);
+        if (error_code) {
             return error_code;
         }
     }
