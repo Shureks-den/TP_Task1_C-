f06286a94aa319bbce3163e146e8c9767c8e2043
Alexander Klonov <alxender.klonov@mail.ru>
2021-03-13 18:47:41 +0300

    Done third test

--- test/test_3_sort.cpp
+++ test/test_3_sort.cpp
@@ -1,22 +1,27 @@
 #include "gtest/gtest.h"
 #include <iostream>
 #include <fstream>
+#include <string>
 
 TEST(Test_algorithm, comparison) {
     
-    std::ifstream t("../build/test/got.txt");
-    char got[2000];
-    t >> got;
+    std::ifstream t("/home/zennoma/Downloads/C++/Task1/build/test/got.txt");
+    std::string got;
+    std::string buff;
+    while(t) {
+        getline(t, buff);
+        got += buff;
+    }
 
-    std::ifstream t2("../expected.txt");
-    char expected[2000];
-    t2 >> expected;
 
-    std::ofstream x ("../result.txt");
-    x << got;
-    x << strlen(expected);
+    std::ifstream t2("/home/zennoma/Downloads/C++/Task1/expected.txt");
+    std::string expected;
+    while(t2) {
+        getline(t2, buff);
+        expected += buff;
+    }
 
     t.close();
     t2.close();
-    ASSERT_STREQ(got, expected);
+    ASSERT_STREQ(got.c_str(), expected.c_str());
 }
